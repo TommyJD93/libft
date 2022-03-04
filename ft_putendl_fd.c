6bc5cafb724942286d76ccd3af3afb3bc246628d
Tommaso <tommy.terribili@gmail.com>
2022-03-04 08:25:15 +0100

    suca

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tterribi <tterribi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tterribi <tterribi@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 19:18:07 by tterribi          #+#    #+#             */
-/*   Updated: 2022/01/19 19:18:09 by tterribi         ###   ########.fr       */
+/*   Updated: 2022/03/04 08:25:02 by tterribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	ft_putendl_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
+	write(1, "suca\n", 5);
 	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 }
